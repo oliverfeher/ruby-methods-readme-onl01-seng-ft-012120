edf439bb536088a07f20e9d7344652cf0be614cb
Oliver Feher <olcsee@gmail.com>
2020-01-22 05:03:55 +0000

    Automatically backed up by Learn

--- lib/example.rb
+++ lib/example.rb
@@ -13,4 +13,17 @@ puts phrase
 puts phrase
 
 def say_hello_world_ten_times
-  
+  puts phrase
+  puts phrase
+  puts phrase
+  puts phrase
+  puts phrase
+  puts phrase
+  puts phrase
+  puts phrase
+  puts phrase
+  puts phrase
+  puts phrase
+end
+
+say_hello_world_ten_times
